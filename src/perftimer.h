175fb3b44124c99e2fe4da3535e28c4f93dc3884
Rene Stange <rsta2@o2online.de>
2022-03-01 20:08:45 +0100

    Add headers to all files

--- src/perftimer.h
+++ src/perftimer.h
@@ -1,6 +1,22 @@
 //
 // perftimer.h
 //
+// MiniDexed - Dexed FM synthesizer for bare metal Raspberry Pi
+// Copyright (C) 2022  The MiniDexed Team
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
 #ifndef _perftimer_h
 #define _perftimer_h
 
