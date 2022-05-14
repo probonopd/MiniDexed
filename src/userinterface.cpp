ac3705ad01e141db10fc8fe40356c0ed2d701339
probonopd <probonopd@users.noreply.github.com>
2022-05-14 22:06:37 +0200

    Try to use i2c display
    https://github.com/probonopd/MiniDexed/issues/169#issuecomment-1126802470

--- src/userinterface.cpp
+++ src/userinterface.cpp
@@ -25,6 +25,8 @@
 #include <string.h>
 #include <assert.h>
 
+#define I2C_MASTER_DEVICE (CMachineInfo::Get ()->GetDevice (DeviceI2CMaster))
+
 LOGMODULE ("ui");
 
 CUserInterface::CUserInterface (CMiniDexed *pMiniDexed, CGPIOManager *pGPIOManager, CConfig *pConfig)
@@ -52,6 +54,9 @@ bool CUserInterface::Initialize (void)
 
 	if (m_pConfig->GetLCDEnabled ())
 	{
+		m_pI2CMaster = new CI2CMaster(I2C_MASTER_DEVICE);
+		m_pLCD = new CHD44780Device (&I2CMaster, CConfig::LCDI2CAddress, CConfig::LCDColumns, CConfig::LCDRows);
+		/* TODO: Read configuration file to see whether i2c should be used for the display. Otherwise run:
 		m_pLCD = new CHD44780Device (CConfig::LCDColumns, CConfig::LCDRows,
 					     m_pConfig->GetLCDPinData4 (),
 					     m_pConfig->GetLCDPinData5 (),
@@ -60,6 +65,7 @@ bool CUserInterface::Initialize (void)
 					     m_pConfig->GetLCDPinEnable (),
 					     m_pConfig->GetLCDPinRegisterSelect (),
 					     m_pConfig->GetLCDPinReadWrite ());
+		*/
 		assert (m_pLCD);
 
 		if (!m_pLCD->Initialize ())
