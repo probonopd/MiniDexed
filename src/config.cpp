bada45d0690a97bed91c12507e92d87a6c5540e6
arsamus <arsamus.web@gmail.com>
2022-10-07 00:16:40 -0300

    Update config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -118,6 +118,7 @@ void CConfig::Load (void)
 	m_bMIDIDumpEnabled  = m_Properties.GetNumber ("MIDIDumpEnabled", 0) != 0;
 	m_bProfileEnabled = m_Properties.GetNumber ("ProfileEnabled", 0) != 0;
 	m_bPerformanceSelectToLoad = m_Properties.GetNumber ("PerformanceSelectToLoad", 1) != 0;
+	m_bAutoBankSkip = m_Properties.GetNumber ("AutoBankSkip", 1) != 0;
 }
 
 const char *CConfig::GetSoundDevice (void) const
@@ -358,4 +359,9 @@ bool CConfig::GetProfileEnabled (void) const
 bool CConfig::GetPerformanceSelectToLoad (void) const
 {
 	return m_bPerformanceSelectToLoad;
+}
+
+bool CConfig::GetAutoBankSkip (void) const
+{
+	return m_bAutoBankSkip;
 }
