16c45c04180cf256e6f778e95acb93c92282d7d2
Vincent <vincent_gauche@hotmail.com>
2023-02-06 14:36:14 +0100

    Adding Diffuser FX

---
+++ src/fx_diffuser.cpp
@@ -0,0 +1,73 @@
+#include "fx_diffuser.h"
+
+#include <cmath>
+#include <algorithm>
+
+#define TAIL , -1
+
+Diffuser::Diffuser(float32_t sampling_rate) : 
+    FXElement(sampling_rate),
+    engine_(sampling_rate)
+{
+    this->engine_.setLFOFrequency(Engine::LFOIndex::LFO_1, 0.5f);
+    this->engine_.setLFOFrequency(Engine::LFOIndex::LFO_2, 0.3f);
+}
+
+Diffuser::~Diffuser()
+{
+}
+
+void Diffuser::reset()
+{
+    this->engine_.reset();
+}
+
+void Diffuser::processSample(float32_t inL, float32_t inR, float32_t& outL, float32_t& outR)
+{
+    typedef Engine::Reserve<126,
+            Engine::Reserve<180,
+            Engine::Reserve<269,
+            Engine::Reserve<444,
+            Engine::Reserve<151,
+            Engine::Reserve<205,
+            Engine::Reserve<245,
+            Engine::Reserve<405> > > > > > > > Memory;
+    Engine::DelayLine<Memory, 0> apl1;
+    Engine::DelayLine<Memory, 1> apl2;
+    Engine::DelayLine<Memory, 2> apl3;
+    Engine::DelayLine<Memory, 3> apl4;
+    Engine::DelayLine<Memory, 4> apr1;
+    Engine::DelayLine<Memory, 5> apr2;
+    Engine::DelayLine<Memory, 6> apr3;
+    Engine::DelayLine<Memory, 7> apr4;
+    Engine::Context c;
+
+    const float32_t kap = 0.625f;
+    float wet = 0.0f;
+
+    engine_.start(&c);
+
+    c.load(inL);
+    c.read(apl1 TAIL, kap);
+    c.writeAllPass(apl1, -kap);
+    c.read(apl2 TAIL, kap);
+    c.writeAllPass(apl2, -kap);
+    c.read(apl3 TAIL, kap);
+    c.writeAllPass(apl3, -kap);
+    c.read(apl4 TAIL, kap);
+    c.writeAllPass(apl4, -kap);
+    c.writeAndLoad(wet, 0.0f);
+    outL = wet;
+
+    c.load(inR);
+    c.read(apr1 TAIL, kap);
+    c.writeAllPass(apr1, -kap);
+    c.read(apr2 TAIL, kap);
+    c.writeAllPass(apr2, -kap);
+    c.read(apr3 TAIL, kap);
+    c.writeAllPass(apr3, -kap);
+    c.read(apr4 TAIL, kap);
+    c.writeAllPass(apr4, -kap);
+    c.writeAndLoad(wet, 0.0f);
+    outR = wet;
+}
---
+++ src/fx_diffuser.h
@@ -0,0 +1,45 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+// fx_shimmer_reverb3.h
+//
+// Stereo Diffuser proposed in the context of the MiniDexed project 
+// It is adapted from the Diffuser that could be found on Cloud EuroRack module from Mutable Instrruments
+//
+#pragma once
+
+#include "fx_components.h"
+#include "fx_engine.hpp"
+
+#define DIFFUSER_BUFFER_SIZE 2048
+
+class Diffuser : public FXElement
+{
+    DISALLOW_COPY_AND_ASSIGN(Diffuser);
+
+public:
+    Diffuser(float32_t sampling_rate);
+    virtual ~Diffuser();
+
+    virtual void reset() override;
+    virtual void processSample(float32_t inL, float32_t inR, float32_t& outL, float32_t& outR) override;
+
+private:
+    typedef FxEngine<DIFFUSER_BUFFER_SIZE, Format::FORMAT_FLOAT32, true> Engine;
+    Engine engine_;
+
+    IMPLEMENT_DUMP()
+    IMPLEMENT_INSPECT(return 0u;)
+};
