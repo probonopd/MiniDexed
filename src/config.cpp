5c050a276fd70658aafbd4a285f8fb07268536a6
donluca <51792528+donluca@users.noreply.github.com>
2023-02-25 16:35:32 +0100

    Update config.cpp

--- src/config.cpp
+++ src/config.cpp
@@ -122,6 +122,7 @@ void CConfig::Load (void)
 	m_bMIDIDumpEnabled  = m_Properties.GetNumber ("MIDIDumpEnabled", 0) != 0;
 	m_bProfileEnabled = m_Properties.GetNumber ("ProfileEnabled", 0) != 0;
 	m_bPerformanceSelectToLoad = m_Properties.GetNumber ("PerformanceSelectToLoad", 1) != 0;
+	m_EngineType = m_Properties.GetString ("EngineType", "MSFA");
 }
 
 const char *CConfig::GetSoundDevice (void) const
@@ -387,4 +388,9 @@ bool CConfig::GetProfileEnabled (void) const
 bool CConfig::GetPerformanceSelectToLoad (void) const
 {
 	return m_bPerformanceSelectToLoad;
+}
+
+const char *CConfig::GetEngineType (void) const
+{
+	return m_EngineType.c_str();
 }
