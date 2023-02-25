657910f6c1d91ee3716849f7a60ba9380a066e8b
donluca <51792528+donluca@users.noreply.github.com>
2023-02-25 16:38:35 +0100

    Added selection for Engine Type

--- src/config.h
+++ src/config.h
@@ -141,6 +141,8 @@ public:
 	
 	// Load performance mode. 0 for load just rotating encoder, 1 load just when Select is pushed
 	bool GetPerformanceSelectToLoad (void) const;
+	const char *GetEngineType (void) const;
+
 
 private:
 	CPropertiesFatFsFile m_Properties;
@@ -208,6 +210,8 @@ private:
 	bool m_bMIDIDumpEnabled;
 	bool m_bProfileEnabled;
 	bool m_bPerformanceSelectToLoad;
+	std::string m_EngineType;
+
 };
 
 #endif
