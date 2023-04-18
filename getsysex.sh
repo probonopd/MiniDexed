a552ba54ac6c0fa5d53aa482680861a3cfd990cc
Luca <51792528+donluca@users.noreply.github.com>
2023-04-18 19:52:41 +0200

    Update getsysex.sh

--- getsysex.sh
+++ getsysex.sh
@@ -12,35 +12,35 @@ TARGET_DIR="sysex/voice/"
 # First put the link to the SysEx file you wish to download, followed by a space and then the filename on Minidexed
 # Please note that files on Minidexed need to start with a number, in this case the next link should be 000028_something.syx
 LINKS=()
-LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom1a.syx 000000_rom1a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom1b.syx 000001_rom1b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom2a.syx 000002_rom2a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom2b.syx 000003_rom2b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc101a.syx 000004_vrc101a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc101b.syx 000005_vrc101b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc102a.syx 000006_vrc102a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc102b.syx 000007_vrc102b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc103a.syx 000008_vrc103a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc103b.syx 000009_vrc103b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc104a.syx 000010_vrc104a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc104b.syx 000011_vrc104b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc105a.syx 000012_vrc105a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc105b.syx 000013_vrc105b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc106a.syx 000014_vrc106a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc106b.syx 000015_vrc106b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc107a.syx 000016_vrc107a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc107b.syx 000017_vrc107b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc108a.syx 000018_vrc108a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc108b.syx 000019_vrc108b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc109a.syx 000020_vrc109a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc109b.syx 000021_vrc109b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc110a.syx 000022_vrc110a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc110b.syx 000023_vrc110b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc111a.syx 000024_vrc111a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc111b.syx 000025_vrc111b.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc112a.syx 000026_vrc112a.syx")
-LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc112b.syx 000027_vrc112b.syx")
-# LINKS+=("https://linkToWebsite.com/something.syx 000028_something.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom1a.syx 00001_rom1a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom1b.syx 00002_rom1b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom2a.syx 00003_rom2a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/factory/rom2b.syx 00004_rom2b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc101a.syx 00005_vrc101a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc101b.syx 00006_vrc101b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc102a.syx 00007_vrc102a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc102b.syx 00008_vrc102b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc103a.syx 00009_vrc103a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc103b.syx 00010_vrc103b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc104a.syx 00011_vrc104a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc104b.syx 00012_vrc104b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc105a.syx 00013_vrc105a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc105b.syx 00014_vrc105b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc106a.syx 00015_vrc106a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc106b.syx 00016_vrc106b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc107a.syx 00017_vrc107a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc107b.syx 00018_vrc107b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc108a.syx 00019_vrc108a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc108b.syx 00020_vrc108b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc109a.syx 00021_vrc109a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc109b.syx 00022_vrc109b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc110a.syx 00023_vrc110a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc110b.syx 00024_vrc110b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc111a.syx 00025_vrc111a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc111b.syx 00026_vrc111b.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc112a.syx 00027_vrc112a.syx")
+LINKS+=("https://yamahablackboxes.com/patches/dx7/vrc/vrc112b.syx 00028_vrc112b.syx")
+# LINKS+=("https://linkToWebsite.com/something.syx 00029_something.syx")
 
 mkdir -p "$TARGET_DIR"
 
@@ -64,6 +64,8 @@ case $? in
         ;;
 esac
 
+declare -i count
+count=1
 # Download all the files in the list
 for i in "${LINKS[@]}"; 
 do
@@ -74,5 +76,11 @@ do
     if (( $? > 0 ))
     then
         echo "Download failed"
+    else
+        count+=1
     fi
 done
+
+# Download the user bank
+printf -v j "%05d" $count
+curl -o "${TARGET_DIR}${j}_userBank.syx" ${CURL_OPTIONS} https://github.com/donluca/MiniDexed/raw/getsysex.sh/userBank.syx > /dev/null 2>&1
