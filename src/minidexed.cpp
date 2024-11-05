8bfba864c0e8cd0a28be8d3ed15cf0979baef212
Gergo Koteles <soyer@irl.hu>
2024-11-05 16:51:23 +0100

    add DAW Controller support for various Arturia controllers
    MiniLab 3
    KeyLab 3 Essential
    KeyLab mkII
    
    based on https://github.com/PrzemekBarski/arturia-keylab-essential-mk3-programming-guide
    
    Tested on a Arturia MiniLab 3 and KeyLab mkII
    
    Keylab 3 Essential is not tested

--- src/minidexed.cpp
+++ src/minidexed.cpp
@@ -1059,6 +1059,21 @@ void CMiniDexed::SetTGParameter (TTGParameter Parameter, int nValue, unsigned nT
 		assert (0);
 		break;
 	}
+
+	switch (Parameter)
+	{
+	case TGParameterCutoff:
+	case TGParameterResonance:
+	case TGParameterPan:
+	case TGParameterReverbSend:
+	case TGParameterMasterTune:
+	case TGParameterPortamentoTime:
+	case TGParameterPortamentoMode:
+			UpdateDAWState ();
+	break;
+	default:
+		break;
+	}
 }
 
 int CMiniDexed::GetTGParameter (TTGParameter Parameter, unsigned nTG)
@@ -1799,6 +1814,25 @@ void CMiniDexed::setMasterVolume (float32_t vol)
 	nMasterVolume=vol;
 }
 
+void CMiniDexed::DisplayWrite (const char *pMenu, const char *pParam, const char *pValue,
+			       bool bArrowDown, bool bArrowUp)
+{
+	m_UI.DisplayWrite (pMenu, pParam, pValue, bArrowDown, bArrowUp);
+
+	for (unsigned i = 0; i < CConfig::MaxUSBMIDIDevices; i++)
+	{
+		m_pMIDIKeyboard[i]->DisplayWrite (pMenu, pParam, pValue, bArrowDown, bArrowUp);
+	}
+}
+
+void CMiniDexed::UpdateDAWState ()
+{
+	for (unsigned i = 0; i < CConfig::MaxUSBMIDIDevices; i++)
+	{
+		m_pMIDIKeyboard[i]->UpdateDAWState ();
+	}
+}
+
 std::string CMiniDexed::GetPerformanceFileName(unsigned nID)
 {
 	return m_PerformanceConfig.GetPerformanceFileName(nID);
@@ -1985,6 +2019,8 @@ void CMiniDexed::LoadPerformanceParameters(void)
 		SetParameter (ParameterReverbLowPass, m_PerformanceConfig.GetReverbLowPass ());
 		SetParameter (ParameterReverbDiffusion, m_PerformanceConfig.GetReverbDiffusion ());
 		SetParameter (ParameterReverbLevel, m_PerformanceConfig.GetReverbLevel ());
+
+		UpdateDAWState ();
 }
 
 std::string CMiniDexed::GetNewPerformanceDefaultName(void)	
