da7c35ff3f1713e3a58face38fe786164a047834
probonopd <probonopd@users.noreply.github.com>
2024-11-16 11:04:49 +0000

    Process the last line of the override file separately
    If it doesn't end with a newline

--- hwconfig/customize.sh
+++ hwconfig/customize.sh
@@ -16,5 +16,14 @@ for file in *.override; do
             sed -i "s/^$key=.*/$key=$value/" "$name_of_ini_file"
         fi
     done < "$file"
+
+    # Process the last line of the override file separately, if it doesn't end with a newline
+    if [ -n "$key" ]; then
+        value=$(echo "$value" | tr -d '\r')
+        if [ -n "$value" ]; then
+            sed -i "s/^$key=.*/$key=$value/" "$name_of_ini_file"
+        fi
+    fi
+
     echo "Created $name_of_ini_file"
 done
