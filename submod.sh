10a2962cc6e309cae1daab0e65e33fe728ae3ea8
probonopd <probonopd@users.noreply.github.com>
2025-03-18 21:38:17 +0100

    git checkout tags/Step49

--- submod.sh
+++ submod.sh
@@ -6,13 +6,13 @@ git submodule update --init --recursive
 #
 # Use fixed master branch of circle-stdlib then re-update
 cd circle-stdlib/
-git checkout tags/Step49
+git checkout 3bd135d
 git submodule update --init --recursive
 cd -
 #
 # Optional update submodules explicitly
 cd circle-stdlib/libs/circle
-git checkout c243194
+git checkout tags/Step49
 cd -
 cd circle-stdlib/libs/circle-newlib
 #git checkout develop
