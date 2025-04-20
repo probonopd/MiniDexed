6910f3ddd4a3f3c3ead55cdefa25095b12746421
probonopd <probonopd@users.noreply.github.com>
2025-04-20 16:18:12 +0200

    Whitespace

--- submod.sh
+++ submod.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -ex
-#
+
 # Update top-level modules as a baseline
 git submodule update --init --recursive
-#
+
 # Use fixed master branch of circle-stdlib then re-update
 cd circle-stdlib/
 git reset --hard
 git checkout 3bd135d
 git submodule update --init --recursive
 cd -
-#
+
 # Optional update submodules explicitly
 cd circle-stdlib/libs/circle
 git reset --hard
@@ -19,7 +19,7 @@ cd -
 cd circle-stdlib/libs/circle-newlib
 #git checkout develop
 cd -
-#
+
 # Use fixed master branch of Synth_Dexed
 cd Synth_Dexed/
 git reset --hard
