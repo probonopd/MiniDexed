9b46ee3e01383dc37bc418086d11e92598b03b09
probonopd <probonopd@users.noreply.github.com>
2025-04-20 19:02:16 +0200

    circle-stdlib for circle Step49

--- submod.sh
+++ submod.sh
@@ -7,18 +7,18 @@ git submodule update --init --recursive
 # Use fixed master branch of circle-stdlib then re-update
 cd circle-stdlib/
 git reset --hard
-git checkout 3bd135d
+git checkout 1111eee # Matches Circle Step49
 git submodule update --init --recursive
 cd -
 
 # Optional update submodules explicitly
-cd circle-stdlib/libs/circle
-git reset --hard
-git checkout tags/Step49
-cd -
-cd circle-stdlib/libs/circle-newlib
+#cd circle-stdlib/libs/circle
+#git reset --hard
+#git checkout tags/Step49
+#cd -
+#cd circle-stdlib/libs/circle-newlib
 #git checkout develop
-cd -
+#cd -
 
 # Use fixed master branch of Synth_Dexed
 cd Synth_Dexed/
