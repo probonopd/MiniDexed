130bb36029aa6df1cf605e23ba5c0f7580fedd09
probonopd <probonopd@users.noreply.github.com>
2025-04-21 17:27:18 +0200

    Remove libwlan.a

--- src/Rules.mk
+++ src/Rules.mk
@@ -30,7 +30,6 @@ LIBS += \
  	$(CIRCLEHOME)/lib/fs/libfs.a \
   	$(CIRCLEHOME)/lib/sched/libsched.a \
 	$(CIRCLEHOME)/lib/libcircle.a \
-	$(CIRCLEHOME)/addon/wlan/libwlan.a \
 	$(CIRCLEHOME)/lib/net/libnet.a
 
 EXTRACLEAN += $(NET_DIR)/*.d $(NET_DIR)/*.o
