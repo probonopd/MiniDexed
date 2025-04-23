e55f23600d8daf463794fc7bcbb46ad451ab2205
probonopd <probonopd@users.noreply.github.com>
2025-04-23 21:06:24 +0200

    Do not comment out Sostenuto and Hold2

--- src/mididevice.cpp
+++ src/mididevice.cpp
@@ -523,19 +523,19 @@ void CMIDIDevice::MIDIMessageHandler (const u8 *pMessage, size_t nLength, unsign
 						case MIDI_CC_BANK_SUSTAIN:
 							m_pSynthesizer->setSustain (pMessage[2] >= 64, nTG);
 							break;
-						/*
+
 						case MIDI_CC_SOSTENUTO:
 							m_pSynthesizer->setSostenuto (pMessage[2] >= 64, nTG);
 							break;
-						*/
+
 						case MIDI_CC_PORTAMENTO:
 							m_pSynthesizer->setPortamentoMode (pMessage[2] >= 64, nTG);
 							break;
-						/*
+
 						case MIDI_CC_HOLD2:
 							m_pSynthesizer->setHoldMode (pMessage[2] >= 64, nTG);
 							break;
-						*/
+
 						case MIDI_CC_RESONANCE:
 							m_pSynthesizer->SetResonance (maplong (pMessage[2], 0, 127, 0, 99), nTG);
 							break;
