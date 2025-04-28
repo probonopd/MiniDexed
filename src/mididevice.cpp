e6aab619206d779bde2f34be6af315a1e8f91097
probonopd <probonopd@users.noreply.github.com>
2025-04-28 22:19:54 +0200

    Simplify code structure
    As suggested by @soyersoyer in https://github.com/probonopd/MiniDexed/pull/896#issuecomment-2836244015

--- src/mididevice.cpp
+++ src/mididevice.cpp
@@ -715,23 +715,22 @@ void CMIDIDevice::HandleSystemExclusive(const uint8_t* pMessage, const size_t nL
       //TODO: add code for storing a bank bulk upload
       LOGNOTE("Currently code  for storing a bulk bank upload is missing!");
       break;
+    case 455:
+      // Parameter 155 + 300 added by Synth_Dexed = 455
+      LOGDBG("Operators enabled: %d%d%d%d%d%d", (pMessage[5] & 0x20) ? 1 : 0, (pMessage[5] & 0x10) ? 1 : 0, (pMessage[5] & 0x08) ? 1 : 0, (pMessage[5] & 0x04) ? 1 : 0, (pMessage[5] & 0x02) ? 1 : 0, (pMessage[5] & 0x01) ? 1 : 0);
+      m_pSynthesizer->setOPMask(pMessage[5], nTG);
+      break;
     default:
       if(sysex_return >= 300 && sysex_return < 500)
       {
-		uint8_t param = pMessage[4] + ((pMessage[3] & 0x03) * 128);
-        if(param == 155) {
-			LOGDBG("Operators enabled: %d%d%d%d%d%d", (pMessage[5] & 0x20) ? 1 : 0, (pMessage[5] & 0x10) ? 1 : 0, (pMessage[5] & 0x08) ? 1 : 0, (pMessage[5] & 0x04) ? 1 : 0, (pMessage[5] & 0x02) ? 1 : 0, (pMessage[5] & 0x01) ? 1 : 0);
-		    m_pSynthesizer->setOPMask(pMessage[5], nTG);
-		} else {
-			LOGDBG("SysEx voice parameter change: Parameter %d value: %d",pMessage[4] + ((pMessage[3] & 0x03) * 128), pMessage[5]);
-			m_pSynthesizer->setVoiceDataElement(pMessage[4] + ((pMessage[3] & 0x03) * 128), pMessage[5],nTG);
-			switch(pMessage[4] + ((pMessage[3] & 0x03) * 128))
-			{
-			case 134:
-				m_pSynthesizer->notesOff(0,nTG);
-				break;
-			}
-     	}
+        LOGDBG("SysEx voice parameter change: Parameter %d value: %d",pMessage[4] + ((pMessage[3] & 0x03) * 128), pMessage[5]);
+        m_pSynthesizer->setVoiceDataElement(pMessage[4] + ((pMessage[3] & 0x03) * 128), pMessage[5],nTG);
+        switch(pMessage[4] + ((pMessage[3] & 0x03) * 128))
+        {
+          case 134:
+            m_pSynthesizer->notesOff(0,nTG);
+            break;
+        }
       }
       else if(sysex_return >= 500 && sysex_return < 600)
       {
