a9aab833a769c07801a91076a4a12e49db501c0a
soyer <soyer@irl.hu>
2025-07-23 22:45:17 +0200

    Fix network
    submod: update circle/addon/wlan/hostap (#962)

--- submod.sh
+++ submod.sh
@@ -6,15 +6,12 @@ git submodule update --init --recursive -f
 
 # Use fixed master branch of circle-stdlib then re-update
 cd circle-stdlib/
-git reset --hard
-git checkout 1111eee -f # Matches Circle Step49
-git submodule update --init --recursive -f
+git checkout -f --recurse-submodules 1111eee # Matches Circle Step49
 cd -
 
 # Optional update submodules explicitly
 cd circle-stdlib/libs/circle
-git reset --hard
-git checkout f18c60fa38042ea7132533e658abfafd5bd63435
+git checkout -f --recurse-submodules f18c60fa38042ea7132533e658abfafd5bd63435
 cd -
 #cd circle-stdlib/libs/circle-newlib
 #git checkout develop
@@ -22,6 +19,5 @@ cd -
 
 # Use fixed master branch of Synth_Dexed
 cd Synth_Dexed/
-git reset --hard
-git checkout 3c683fc801 -f
+git checkout -f 3c683fc801
 cd -
