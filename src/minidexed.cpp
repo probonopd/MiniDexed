c507cb0a88cb62bf9509200ed8336c50299e3174
Gergo Koteles <soyer@irl.hu>
2025-07-27 17:53:57 +0200

    CMiniDexed: show Network Ready + IP instead of TG1
    TG1 is misleading if the menu is not on it.
    
    Show only for for 3 seconds.

--- src/minidexed.cpp
+++ src/minidexed.cpp
@@ -2258,6 +2258,14 @@ unsigned CMiniDexed::getModController (unsigned controller, unsigned parameter, 
 	
 }
 
+static void UpdateScreen(TKernelTimerHandle hTimer, void *pParam, void *pContext)
+{
+	CUserInterface *pUI = static_cast<CUserInterface *> (pContext);
+	assert (pUI);
+
+	pUI->DisplayChanged ();
+}
+
 void CMiniDexed::UpdateNetwork()
 {
 	if (!m_pNet) {
@@ -2300,7 +2308,8 @@ void CMiniDexed::UpdateNetwork()
 			LOGNOTE("FTP daemon not started (NetworkFTPEnabled=0)");
 		}
 
-		m_UI.DisplayWrite (IPString, "", "TG1", 0, 1);
+		m_UI.DisplayWrite (IPString, "", "Network ready", 0, 1);
+		CTimer::Get ()->StartKernelTimer (MSEC2HZ (3000), UpdateScreen, 0, &m_UI);
 
 		m_pmDNSPublisher = new CmDNSPublisher (m_pNet);
 		assert (m_pmDNSPublisher);
